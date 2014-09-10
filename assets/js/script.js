5bdd20fdcdcb0c5b2ad7d9b38ca8ca0a21872754
Jure Cuhalev <gandalfar@gmail.com>
2014-09-10 19:23:55 +0200

    homepage slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function($) {
-	
+
+    $(".rslides").responsiveSlides({
+      auto: true,
+      speed: 2000,
+      timeout: 8000
+    });
+
 });
--- home.php
+++ home.php
@@ -1,7 +1,46 @@
 <?php get_header(); ?>
 
-<!-- 	<div class="slide">
-		<img src="<?php echo get_template_directory_uri(); ?>/img/nature.jpg" />
-	</div>
- -->
+<?php $custom_query = new WP_Query(array(
+									'post_type' => 'slideshow',
+									'ignore_sticky_posts' => 1,
+									'orderby' => 'rand'
+									// 'posts_per_page' => 1
+									));
+?>
+	<ul class="rslides">
+		<?php while ( $custom_query->have_posts() ) : $custom_query->the_post(); ?>
+			<?php $image_list = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slideshow-image' );
+
+				if (count($image_list) > 0) {
+					$image_src = $image_list[0];
+				}
+			?>
+
+			<li class="slide">
+				<?php if ( get_field('slideshow_url') ) : ?>
+					<a href="<?php the_field('slideshow_url'); ?>">
+						<img src="<?php echo $image_src; ?>" />
+					</a>
+				<?php else: ?>
+					<img src="<?php echo $image_src; ?>" />
+				<?php endif; ?>
+
+				<?php if ( get_field('slideshow_show_description') ) : ?>
+				<a class="card"
+					href="get_field('slideshow_url')"; >
+					<span class="card-title"><?php the_title(); ?></span>
+					<?php if ( get_field('slideshow_description') ) : ?>
+						<span class="card-description"><?php the_field('slideshow_description'); ?></span>
+					<?php endif; ?>
+				<a>
+				<div class="attribution">
+					<?php if ( get_field('slideshow_photo_license_text') ) :?>
+						<a href="<?php the_field('slideshow_photo_source_url'); ?>" ><?php the_field('slideshow_photo_license_text'); ?></a>
+					<?php endif; ?>
+				</div>
+				<?php endif; ?>
+			</li>
+		<?php endwhile; ?>
+	</ul>
+
 <?php get_footer(); ?>

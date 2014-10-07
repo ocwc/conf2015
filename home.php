442093007cbb603ca464a1b1f992cda27209303e
Jure Cuhalev <gandalfar@gmail.com>
2014-10-07 19:06:36 +0200

    social share on home page

--- home.php
+++ home.php
@@ -76,8 +76,14 @@
 </div>
 
 <div class="container home-vertical-spacing">
-	<div class="col-sm-12">
+	<div class="col-sm-10">
 		<h2>Join the Conversation by using <span class="hashtag">#oeglobal</span> hashtag</h2>
+	</div>
+	<div class="col-sm-2">
+		<ul class="share-buttons pull-right">
+			<li><a href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fconference.oeconsortium.org%2F2015%2F&text=OEC%20Global%202015%20%23oecglobal:%20http%3A%2F%2Fconference.oeconsortium.org%2F2015%2F&via=oeconsortium" target="_blank" title="Tweet"><img src="<?php echo get_template_directory_uri(); ?>/img/flat_web_icon_set/black/Twitter.png"></a></li>
+			<li><a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fconference.oeconsortium.org%2F2015%2F&t=OEC%20Global%202015%20%23oecglobal" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/flat_web_icon_set/black/Facebook.png"></a></li>
+		</ul>
 	</div>
 
 	<div class="col-sm-12">

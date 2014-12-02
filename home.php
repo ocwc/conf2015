5fba431da6e11f311759c01e978ffb007dbd0b65
Jure Cuhalev <gandalfar@gmail.com>
2014-12-02 12:46:04 +0100

    embeds

--- home.php
+++ home.php
@@ -96,7 +96,7 @@
 	</div>
 
 	<div class="col-sm-12">
-		<iframe id="storyfeed" src="http://oeglobal.storyfeed.me/?embed" style="border: 0px; height: 300px; width: 100%;" scrolling="no" allowfullscreen></iframe>
+		<iframe id="storyfeed" src="http://oeglobal.storyfeed.me/?embed&maxposts=9" style="border: 0px; height: 300px; width: 100%;" scrolling="no" allowfullscreen></iframe>
 	</div>
 </div>
 

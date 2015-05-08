93bbe8f813803085b914f311260794aed23f582f
Jure Cuhalev <gandalfar@gmail.com>
2015-05-08 22:22:19 +0100

    add indicators

--- partials/_presentation-excerpt.php
+++ partials/_presentation-excerpt.php
@@ -1,7 +1,17 @@
 <tr>
-	<td>
+	<td class="col-sm-6">
 		<?php the_field('presentation_author'); ?> <br />
 		<em><?php the_field('presentation_institution'); ?></em>
 	</td>
-	<td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></td>
+	<td class="col-sm-6">
+		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		<br />
+		<?php if ( get_field( 'presentation_pdf' ) ) : ?>
+			<i class="icon-doc-text"></i>
+		<?php endif; ?>
+
+		<?php if ( get_field( 'presentation_slideshare' ) ) : ?>
+			<i class="icon-slideshare"></i>
+		<?php endif; ?>
+	</td>
 </tr>

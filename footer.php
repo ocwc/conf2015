7d467c4645a68dcad64b0e9d83c6569a641bd64d
Jure Cuhalev <gandalfar@gmail.com>
2015-07-16 15:40:58 +0100

    update footer text

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
       <a class="col-sm-2 col-md-1 col-xs-12 license-logo" rel="license" href="http://creativecommons.org/licenses/by/4.0/">
         	<img alt="Creative Commons License" style="border-width:0" src="https://licensebuttons.net/l/by/4.0/80x15.png">
       </a>
-      <span class="col-sm-10 col-md-11 col-xs-12 license-text">All content on oeconsortium.org is licensed under <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</span>
+      <span class="col-sm-10 col-md-11 col-xs-12 license-text">Except where otherwise noted, content on this site is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</a>.</span>
     </div>
 	</div>
 <?php wp_footer(); ?>

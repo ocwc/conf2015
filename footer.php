922e3fd58b5f9b236e547afb65e2021827a012f2
Jure Cuhalev <gandalfar@gmail.com>
2015-11-03 23:23:00 +0200

    Add GA code

--- footer.php
+++ footer.php
@@ -8,7 +8,18 @@
       <span class="col-sm-10 col-md-11 col-xs-12 license-text">Except where otherwise noted, content on this site is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</span>
     </div>
 	</div>
+
 <?php wp_footer(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-4248822-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 </body>
 </html>

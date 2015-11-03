5535d875a107b4bb5bef7e2ea2dbb7c87738a9a0
Jure Cuhalev <gandalfar@gmail.com>
2015-11-03 19:54:47 +0200

    awards

---
+++ partials/home-awards.php
@@ -0,0 +1,41 @@
+<div class="awards-container">
+  <div class="container home-vertical-spacing boffset-35">
+    <div class="row">
+      <div class="col-sm-4 col-sm-offset-1">
+        <img class="img-responsive" src="<?php echo get_template_directory_uri() . '/img/awards/badge-oeawards-2016.png'; ?>">
+      </div>
+      <div class="col-sm-7">
+        <div class="col-sm-9 col-sm-offset-2 text-center voffset-20 boffset-20">
+          <img class="img-responsive"
+               src="<?php echo get_template_directory_uri() . '/img/awards/oew-awards-2016-laurel.png'; ?>">
+        </div>
+
+        <div class="col-sm-6 voffset-10 boffset-10">
+          <img class="img-responsive"
+               src="<?php echo get_template_directory_uri() . '/img/awards/oew-awards-educators-award-2016-laurel.png'; ?>">
+        </div>
+
+        <div class="col-sm-6 voffset-10 boffset-10">
+          <img class="img-responsive"
+              src="<?php echo get_template_directory_uri() . '/img/awards/oew-awards-leadership-award-2016-laurel.png'; ?>">
+        </div>
+
+        <div class="col-sm-6 voffset-10 boffset-10">
+          <img class="img-responsive"
+               src="<?php echo get_template_directory_uri() . '/img/awards/oew-awards-open-research-2016-laurel.png'; ?>">
+        </div>
+
+        <div class="col-sm-6 voffset-10 boffset-10">
+          <img class="img-responsive"
+              src="<?php echo get_template_directory_uri() . '/img/awards/oew-awards-outstanding-course-2016-laurel.png'; ?>">
+        </div>
+
+        <div class="col-sm-12">
+          <div class="text-center voffset-20">
+            <a href="http://www.oeconsortium.org/projects/open-education-awards-for-excellence/" class="btn-gray">Learn about Awards</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ partials/home-cfp.php
@@ -0,0 +1,28 @@
+<div class="container home-vertical-spacing boffset-35">
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <h2 class="grayLight">Call for Proposals</h2>
+    </div>
+
+    <div class="col-sm-10 col-sm-offset-1 text-center">
+      <p>The Open Education Consortium and the AGH University of Science and Technology, organizers of the Open Education Global Conference 2016, invite proposals for conference sessions to take place in Kraków, Poland April 12-14. <br />The theme of the Open Education Global Conference 2016 is Convergence Through Collaboration.</p>
+    </div>
+
+    <div class="col-sm-5 col-sm-offset-4">
+      <ul class="list-unstyled list-checked">
+        <li>Integration of Open Practices</li>
+        <li>Collaboration</li>
+        <li>Research to Advance Open Education</li>
+        <li>Open Education Initiatives in Europe</li>
+      </ul>
+    </div>
+
+    <div class="col-sm-12 voffset-30 text-center">
+      <a class="btn-blue" href="/2016/call-for-proposals/">View More Details</a>
+      <a class="btn-blue" href="https://easychair.org/conferences/?conf=oeglobal2016">Submit Proposal</a>
+      <a class="btn-blue" href="/2016/call-for-proposals/submission-guidelines/">Submission Guidelines</a>
+      <a class="btn-blue" href="/2016/call-for-proposals/journal-publication-opportunity/">Journal Publication Opportunity</a>
+    </div>
+
+  </div>
+</div>

6da93a11cbeacab485d732028a9285a3fd6d1cca
Jure Cuhalev <gandalfar@gmail.com>
2016-06-06 22:03:51 +0200

    Attribution

--- partials/home-featured.php
+++ partials/home-featured.php
@@ -1,7 +1,9 @@
 <div class="home-featured">
   <div class="container text-center text-uppercase">
     <h1 class="featured-h1">Call for Proposals</h1>
-    <h2 class="featured-h2">8 &mdash; 10 March, 2017 / Cape Town, South Africa</h2>
+    <h2 class="featured-h2">8 &mdash; 10 March, 2017 <span class="hidden-xs">/</span><br class="visible-xs" /> Cape Town, South Africa</h2>
     <a href="#" class="btn btn-orange btn-featured text-uppercase">Submit Yours</a>
+
+    <a href="https://www.flickr.com/photos/blyzz/2667536959/" class="featured-attribution" target="_blank">Photo by: Jim Sher</a>
   </div>
 </div>

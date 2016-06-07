e124827d65dcd27e5c88b09a7532807da85a1a53
Jure Cuhalev <gandalfar@gmail.com>
2016-06-07 17:20:57 +0200

    Awards on home page

--- partials/home-awards.php
+++ partials/home-awards.php
@@ -1,40 +1,19 @@
-<div class="awards-container hidden-xs">
+<div class="awards-container">
   <div class="container">
-    <div class="row">
-      <div class="col-sm-4 col-sm-offset-1">
-        <img class="img-responsive" src="<?php echo get_template_directory_uri() . '/img/awards/badge-oeawards-2016.png'; ?>">
+    <div class="row voffset-35">
+      <div class="col-sm-4 col-sm-offset-4 text-center">
+        <img class="img-responsive" src="<?php echo get_template_directory_uri() . '/img/awards/badge-oeawards-2017.png'; ?>">
       </div>
-      <div class="col-sm-7">
-        <div class="col-sm-9 col-sm-offset-2 text-center voffset-20 boffset-20">
-          <img class="img-responsive"
-               src="<?php echo get_template_directory_uri() . '/img/awards/oew-awards-2016-laurel.png'; ?>">
+      <div class="col-sm-8 col-sm-offset-2 awards-text voffset-35">
+        <p class="u-text-white">
+          The Open Education Awards for Excellence provide annual recognition to outstanding contributions in the Open Education Consortium community. These awards recognize distinctive open education sites, technological innovations, notable open courses, and exemplary leaders in Open Education worldwide.
+        </p>
+      </div>
+      <div class="col-sm-4 col-sm-offset-4">
+        <div class="text-center voffset-20 boffset-35">
+          <a href="http://www.oeconsortium.org/projects/open-education-awards-for-excellence/" class="btn-white btn-block" target="_blank">Submit Nomination</a>
         </div>
-
-        <div class="col-sm-6 voffset-10 boffset-10">
-          <img class="img-responsive"
-               src="<?php echo get_template_directory_uri() . '/img/awards/oew-awards-educators-award-2016-laurel.png'; ?>">
-        </div>
-
-        <div class="col-sm-6 voffset-10 boffset-10">
-          <img class="img-responsive"
-              src="<?php echo get_template_directory_uri() . '/img/awards/oew-awards-leadership-award-2016-laurel.png'; ?>">
-        </div>
-
-        <div class="col-sm-6 voffset-10 boffset-10">
-          <img class="img-responsive"
-               src="<?php echo get_template_directory_uri() . '/img/awards/oew-awards-open-research-2016-laurel.png'; ?>">
-        </div>
-
-        <div class="col-sm-6 voffset-10 boffset-10">
-          <img class="img-responsive"
-              src="<?php echo get_template_directory_uri() . '/img/awards/oew-awards-outstanding-course-2016-laurel.png'; ?>">
-        </div>
-
-        <div class="col-sm-12">
-          <div class="text-center voffset-20">
-            <a href="http://www.oeconsortium.org/projects/open-education-awards-for-excellence/" class="btn-gray">Learn about Awards</a>
-          </div>
-        </div>
+      </div>
       </div>
     </div>
   </div>

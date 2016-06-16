e51d74f8c2f14a395550dab60d73f1bab495ec89
Jure Cuhalev <gandalfar@gmail.com>
2016-06-16 16:06:33 +0200

    Update link

--- partials/home-featured.php
+++ partials/home-featured.php
@@ -2,7 +2,7 @@
   <div class="container text-center text-uppercase">
     <h1 class="featured-h1">Call for Proposals</h1>
     <h2 class="featured-h2">8 &mdash; 10 March, 2017 <span class="hidden-xs">/</span><br class="visible-xs" /> Cape Town, South Africa</h2>
-    <a href="#" class="btn btn-orange btn-featured text-uppercase">Submit Yours</a>
+    <a href="/2017/call-for-proposals/" class="btn btn-orange btn-featured text-uppercase">Submit Your Proposal</a>
 
     <a href="https://www.flickr.com/photos/blyzz/2667536959/" class="featured-attribution" target="_blank">Photo by: Jim Sher</a>
   </div>

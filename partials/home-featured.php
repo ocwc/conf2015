cd49ec652fe602f8c7440ab1c54bb500c2af56e0
Jure Cuhalev <jure@cuhalev.com>
2017-03-02 08:35:15 +0100

    Add schedule to home page

--- partials/home-featured.php
+++ partials/home-featured.php
@@ -1,8 +1,8 @@
 <div class="home-featured">
   <div class="container text-center text-uppercase">
-    <h1 class="featured-h1">Registration is Open</h1>
+    <h1 class="featured-h1">Program is now available</h1>
     <h2 class="featured-h2">8 &mdash; 10 March, 2017 <span class="hidden-xs">/</span><br class="visible-xs" /> Cape Town, South Africa</h2>
-    <a href="/2017/registration/" class="btn btn-orange btn-featured text-uppercase">Register here</a>
+    <a href="/2017/schedule/" class="btn btn-orange btn-featured text-uppercase">View Program</a>
 
     <a href="https://www.flickr.com/photos/blyzz/2667536959/" class="featured-attribution" target="_blank">Photo by: Jim Sher</a>
   </div>
